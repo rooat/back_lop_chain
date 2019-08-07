d4da8786fd7c456a554659f6bf2a19ce0798c9cc
rooat <welling1234@gmail.com>
2019-08-07 15:48:38 +0800

    u

--- lop/config/contract.js
+++ lop/config/contract.js
@@ -1,7 +1,7 @@
 var Web3 = require('web3');
 var web3 = new Web3();
-//const ContractAddr = '0x15ffac6aa02a6374a2748ac945fc92116c41fac5';
-const ContractAddr ='0x14b0469cc515096164ac499d08fee263105129ce'
+const ContractAddr = '0x8e43127314a852e357bb50b58b7fad7a457a5e9f';
+//const ContractAddr ='0x14b0469cc515096164ac499d08fee263105129ce'
 
 // const MasterNodoContractAddr = '0x8e43127314a852e357bb50b58b7fad7a457a5e9f';
 const DeployBlock = 19912145;
