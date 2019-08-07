fb0093a2acf8c50539540f71a4bb4b78cfa6da07
rooat <welling1234@gmail.com>
2019-08-07 15:15:04 +0800

    u

--- config.js
+++ config.js
@@ -16,14 +16,16 @@ var tokenAddress= "0xbc923f769f56493230fdc45db5ca43742862fb20";//合约地址
 const token = require('./contract/tokenABI.json');
 
 
-var sender = "0xdCb25d3710645C2fe3Fd7e04601De50bD54ed124";
+//var sender = "0xdCb25d3710645C2fe3Fd7e04601De50bD54ed124";
+var sender = "0xDfaB41faf59A7235cB9c4574D5aE4ca0FD5227D3"
 
 var Web3 = require('web3');
 var net = require('net');
  var web3 = new Web3('/home/ubuntu/.etherzero/geth.ipc', net);
 
 //   var  web3 = new Web3('http://etzrpc.org');
-
+//contract test = "0x14b0469cc515096164ac499d08fee263105129ce"
+//sender test = "0xDfaB41faf59A7235cB9c4574D5aE4ca0FD5227D3"
 module.exports = {
 
     utils,
